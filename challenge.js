307078875684325f09e3410c4fa6e14f8ccad6e5
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-11 18:19:10 +0300

    done!!!!!!!

--- challenge.js
+++ challenge.js
@@ -1,2 +1,16 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function playGame(player1, player2) {
+    if (player1 === player2) {
+        return "tie";
+    } else if (
+        (player1 === "rock" && player2 === "scissors") ||
+        (player1 === "scissors" && player2 === "paper") ||
+        (player1 ==="paper" && player2 === "rock")
+    ) {
+        return "player1 wins";
+    } else {
+        return "player2 wins";
+    }
+}
+console.log(playGame("scissors", "paper"));
